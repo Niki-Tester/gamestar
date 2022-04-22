1cce6a7dd95f265c6bc15359507fba196fe57150
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 13:54:17 +0000

    feat: adds app.py file

---
+++ app.py
@@ -0,0 +1,6 @@
+"""
+GameStar - A web application for users to find reviews
+on video games they would like to play, and leave reviews
+on games they have played.
+Full readme available at: https://github.com/Niki-Tester/gamestar
+"""
