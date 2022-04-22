f86818faad5716a08d4cc5378d1a60124fe2dbf9
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 13:58:25 +0000

    feat: setup app config for use with mongodb

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ Full readme available at: https://github.com/Niki-Tester/gamestar
 
 import os
 from flask import Flask
+
 if os.path.exist('env.py'):
     # flake8: noqa: f401 pylint: disable = unused-import
     import env
 
 app = Flask(__name__)
+
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
