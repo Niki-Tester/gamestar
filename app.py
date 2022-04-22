d94f1efa267ac257e5b273882ba054ca2006fdf9
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 14:24:16 +0000

    feat: add app.run

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Full readme available at: https://github.com/Niki-Tester/gamestar
 
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
 
 if os.path.exists('env.py'):
     # flake8: noqa: f401 pylint: disable = unused-import
@@ -17,7 +18,7 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 
-mongo = Pymongo(app)
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
@@ -25,3 +26,8 @@ def index():
     Display test page
     """
     return 'Hello Gamers!'
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
