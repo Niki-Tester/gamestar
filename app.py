f1481a71395ab4fe2f285f679df4cdf9964a045c
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 13:56:16 +0000

    feat: import os and env

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ on video games they would like to play, and leave reviews
 on games they have played.
 Full readme available at: https://github.com/Niki-Tester/gamestar
 """
+
+import os
+
+if os.path.exist('env.py'):
+    # flake8: noqa: f401 pylint: disable = unused-import
+    import env
