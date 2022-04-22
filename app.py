b88b0fe10d5997abffd425433170280e256ece31
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 14:11:57 +0000

    fix: typo if os.path.exist to if os.path.exists

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Full readme available at: https://github.com/Niki-Tester/gamestar
 import os
 from flask import Flask
 
-if os.path.exist('env.py'):
+if os.path.exists('env.py'):
     # flake8: noqa: f401 pylint: disable = unused-import
     import env
 
