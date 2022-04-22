185af05884a8942690bf9de3d83b68ed5b03a774
Niki Tester <bloatedniknak@gmail.com>
2022-04-22 13:57:26 +0000

    feat: setup up flask app

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ Full readme available at: https://github.com/Niki-Tester/gamestar
 """
 
 import os
-
+from flask import Flask
 if os.path.exist('env.py'):
     # flake8: noqa: f401 pylint: disable = unused-import
     import env
+
+app = Flask(__name__)
