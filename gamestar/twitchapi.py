473dc4083efad4215ca63cfd89fd411ecb77ff6e
Niki Tester <bloatedniknak@gmail.com>
2022-05-05 21:58:09 +0000

    feat: add twitch access_token to database

---
+++ gamestar/twitchapi.py
@@ -0,0 +1,49 @@
+"""
+The TwitchAPI is used to obtain an access token.
+The access token is stored in a mongoDB database.
+The access token has an expiry date, and must be
+updated before the token expires.
+"""
+import os
+import time
+import math
+import requests
+from gamestar.models import Twitch
+from gamestar import db
+
+
+def new_access_token():
+    """
+    Makes a post request to the twitch API to
+    obtain an access token
+    """
+    url = 'https://id.twitch.tv/oauth2/token?'\
+        f'client_id={os.environ.get("CLIENT_ID")}'\
+        f'&client_secret={os.environ.get("CLIENT_SECRET")}'\
+        '&grant_type=client_credentials'
+
+    result = requests.post(url)
+    data = result.json()
+
+    existing_token = Twitch.query.get(1)
+
+    if existing_token:
+        existing_token.access_token = data['access_token']
+        existing_token.expires_in = data['expires_in']
+        existing_token.token_type = data['token_type']
+        existing_token.date_added = math.floor(time.time())
+
+        # pylint: disable = no-member
+        db.session.add(existing_token)
+        db.session.commit()
+    else:
+        token = Twitch(
+            access_token=data['access_token'],
+            expires_in=data['expires_in'],
+            token_type=data['token_type'],
+            date_added=math.floor(time.time())
+        )
+
+        # pylint: disable = no-member
+        db.session.add(token)
+        db.session.commit()
