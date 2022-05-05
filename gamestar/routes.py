ff240feeff3514052183cd745730adc5a8264aed
Niki Tester <bloatedniknak@gmail.com>
2022-05-05 11:17:31 +0000

    feat: add user to database functionality

--- gamestar/routes.py
+++ gamestar/routes.py
@@ -1,8 +1,10 @@
 """Application Routes"""
 
+import re
 from flask import render_template, request, redirect, url_for
 from gamestar import app, db
 from gamestar.models import User, Game, Review
+from werkzeug.security import generate_password_hash
 
 
 @app.route('/')
@@ -20,4 +22,38 @@ def register():
     POST: Get form data for user registration. 
     If valid, store in User table in database.
     """
+    if request.method == 'POST':
+        username = request.form.get('username').lower().strip()
+        password = request.form.get('password')
+        password_confirm = request.form.get('confirm_password')
+
+        print(request.form.get('username').lower().strip())
+
+        if len(username) < 3 or len(username) > 16:
+            print('Username not of required length')
+            return render_template('register.html')
+
+        if re.search('[^A-z0-9]', username):
+            print(re.search('[^A-z0-9]', username))
+            print('Invalid characters in username')
+            return render_template('register.html')
+
+        if password != password_confirm:
+            print('Password mismatch')
+            return render_template('register.html')
+
+        existing_user = User.query.filter(User.username == username).count()
+
+        if existing_user > 0:
+            print('User name already exists in DB')
+            return render_template('register.html')
+
+        password_hash = generate_password_hash(password)
+
+        user = User(
+            username=username, password=password_hash
+            )
+        db.session.add(user)
+        db.session.commit()
+
     return render_template('register.html')
