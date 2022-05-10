fe563da25db9ac3f1f94864b83d1a648bd721812
Niki Tester <bloatedniknak@gmail.com>
2022-05-10 13:34:24 +0000

    feat: dynamically style rating stars based on range value

---
+++ gamestar/static/js/ratingSlider.js
@@ -0,0 +1,12 @@
+const slider = document.getElementById('review-rating');
+slider.addEventListener('input', updateRating)
+
+const rating_inner = document.querySelector('.rating-inner');
+
+function updateRating() {
+    rating_inner.style.width = `${Math.round(mapNum(slider.value, 0, 5, 0, 100))}%`
+}
+
+function mapNum(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
