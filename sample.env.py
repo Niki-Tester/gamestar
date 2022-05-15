97bb5020ac83187fe3881ba529adb868f9ed7f56
Niki Tester <bloatedniknak@gmail.com>
2022-05-15 10:59:10 +0000

    doc: sample environment variables

---
+++ sample.env.py
@@ -0,0 +1,14 @@
+"""
+EXAMPLE: Environment variabled for Gamestar application
+"""
+import os
+
+os.environ.setdefault('IP', '0.0.0.0')
+os.environ.setdefault('PORT', '5566')
+os.environ.setdefault('DEBUG', 'True')
+os.environ.setdefault('SECRET_KEY', '<A_SECURE_PASSWORD>')
+os.environ.setdefault('DB_URL', 'postgresql://<DB_USERNAME>:<DB_PASSWORD>'
+                      '@<DB_HOST>:<DB_PORT>/<DB_NAME')
+
+os.environ.setdefault('CLIENT_ID', '<TWITCH_CLIENT_ID>')
+os.environ.setdefault('CLIENT_SECRET', '<TWITCH_CLIENT_SECRET>')
