d7ba34643ed8b3f1c8757213ce87a797522ebdee
Niki Tester <bloatedniknak@gmail.com>
2022-05-18 10:51:23 +0000

    fix: SQL DB URI incorrect host string on heroku

--- gamestar/__init__.py
+++ gamestar/__init__.py
@@ -15,7 +15,8 @@ if os.path.exists('env.py'):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')[:8]\
+                                + 'ql' + os.environ.get('DATABASE_URL')[8:]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
