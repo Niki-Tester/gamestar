1d04f020c7de099c153721f93626ba347d6d69c6
Niki Tester <bloatedniknak@gmail.com>
2022-05-20 17:25:53 +0000

    feat: update review like counter

--- gamestar/static/js/likesCounter.js
+++ gamestar/static/js/likesCounter.js
@@ -1,21 +1,33 @@
 likeButtons = document.getElementsByClassName('like-btn')
 
+let gAwaitingResult = false;
+
 for (const likeButton of likeButtons) {
-    likeButton.addEventListener('click', e => {
-        e.target.children[0].classList.toggle('liked')
+    likeButton.addEventListener('click', toggleLike)
+}
 
-        const formData = new FormData();
-        formData.append("review_id", e.target.dataset.reviewId);
+function toggleLike(e) {
+    e.preventDefault()
+    likeCounter = document.getElementById(`review-counter-${e.target.dataset.reviewId}`)
 
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                likeButton.children[1].innerText = xhttp.responseText
-            }
-        };
+    const formData = new FormData();
+    formData.append("review_id", e.target.dataset.reviewId);
 
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            e.target.children[0].classList.toggle('liked')
+            e.target.children[1].innerText = xhttp.responseText
+            likeCounter.innerText = xhttp.responseText
+            gAwaitingResult = false
+        }
+    };
+
+    if (!gAwaitingResult) {
+        gAwaitingResult = true;
         xhttp.open("POST", "/likes", true);
         xhttp.send(formData);
-        likeButton.disabled = true
-    })
+    } else {
+        console.log('Awaiting Previous Response');
+    }
 }
