65c7ea152647dae4e0e780a6c08edc9771ff9ffb
Niki Tester <bloatedniknak@gmail.com>
2022-05-25 20:10:48 +0000

    fix: debug option not evaluating as bool

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import os
 from gamestar import app
 
-
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        debug=eval(os.environ.get('DEBUG'))
     )
