abcfdfe855e5f7b356c48e6fa99df7f0cee68e7b
Niki Tester <bloatedniknak@gmail.com>
2022-06-02 12:29:03 +0000

    doc: change DB_URL env variable to DATABASE_URL

--- sample.env.py
+++ sample.env.py
@@ -7,7 +7,7 @@ os.environ.setdefault('IP', '0.0.0.0')
 os.environ.setdefault('PORT', '5566')
 os.environ.setdefault('DEBUG', 'True')
 os.environ.setdefault('SECRET_KEY', '<A_SECURE_PASSWORD>')
-os.environ.setdefault('DB_URL', 'postgres://<DB_USERNAME>:<DB_PASSWORD>'
+os.environ.setdefault('DATABASE_URL', 'postgres://<DB_USERNAME>:<DB_PASSWORD>'
                       '@<DB_HOST>:<DB_PORT>/<DB_NAME')
 
 os.environ.setdefault('CLIENT_ID', '<TWITCH_CLIENT_ID>')
